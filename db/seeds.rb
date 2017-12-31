637cf89a754099639ad8875a412ce9470241bd61
kevin robert <kevin.j.robert@gmail.com>
2017-12-31 18:18:21 +0100

    simpsons seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,19 +4,19 @@ Message.destroy_all
 User.destroy_all
 Channel.destroy_all
 
-general = Channel.create(name: 'general')
-paris = Channel.create(name: 'paris')
-react = Channel.create(name: 'react')
+general = Channel.create!(name: 'general')
+paris = Channel.create!(name: 'paris')
+react = Channel.create!(name: 'react')
 
 
-kevin = User.create(email: 'kevin@chat.com', password: '1234')
-ed = User.create(email: 'ed@chat.com', password: '1234')
-seb = User.create(email: 'seb@chat.com', password: '1234')
+kevin = User.create!(email: 'kevin@chat.com', password: '123456')
+ed = User.create!(email: 'ed@chat.com', password: '123456')
+seb = User.create!(email: 'seb@chat.com', password: '123456')
 
 Channel.find_each do |chan|
   10.times do
     user = User.all.sample
-    Message.create(channel: chan, user: user, content: Faker::Simpsons.quote)
+    Message.create!(channel: chan, user: user, content: Faker::Simpsons.quote)
   end
 end
 
